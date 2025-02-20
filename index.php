bfd2c4f6fb43a243a4c34483ccdbc6c969a98107
Andhika <syahrulananda04@gmail.com>
2025-02-20 09:46:03 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Git Remote</title>
 </head>
+
 <body>
     <h1>Hello Wolrd!!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, praesentium esse dolorem eius nisi nam perferendis ullam quos adipisci dignissimos, impedit sed ex soluta hic accusamus totam! Placeat, ipsa magni?</p>
+    <ul>
+        <li>3</li>
+        <li>3</li>
+        <li>3</li>
+    </ul>
 </body>
+
 </html>
